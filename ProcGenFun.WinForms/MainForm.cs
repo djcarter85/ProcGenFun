57931ce5f5b72dc8a41a57b4d1364760aa86d16c
Dan Carter <djcarter85@gmail.com>
2024-12-08 20:36:01 +0000

    feat: add main form as entry point

---
+++ ProcGenFun.WinForms/MainForm.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcGenFun.WinForms;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void VisualiseDistributionsButton_Click(object sender, EventArgs e)
+    {
+        new DistributionsForm().Show();
+    }
+}
