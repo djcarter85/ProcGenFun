9dee4aa7c114160d663f79eb05e3107e2bd98587
Dan Carter <djcarter85@gmail.com>
2024-12-13 20:52:35 +0000

    feat: add implementation of binary tree

---
+++ ProcGenFun/UniformDistribution.cs
@@ -0,0 +1,26 @@
+﻿namespace ProcGenFun;
+
+using RandN;
+using RandN.Distributions;
+using RandN.Extensions;
+
+public static class UniformDistribution
+{
+    public static bool TryCreate<T>(IEnumerable<T> items, out IDistribution<T> dist)
+    {
+        var list = items.ToList();
+
+        if (list.Count == 0)
+        {
+            dist = default!;
+            return false;
+        }
+        else
+        {
+            dist =
+                from index in Uniform.NewInclusive(0, list.Count - 1)
+                select list[index];
+            return true;
+        }
+    }
+}
