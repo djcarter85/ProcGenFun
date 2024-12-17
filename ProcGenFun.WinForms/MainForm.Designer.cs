djcarter85/FunProcGen

feat: add initial projects

FunProcGen.WinForms/MainForm.Designer.cs
@@ -1,0 +1,39 @@
+﻿namespace FunProcGen.WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Fun ProcGen";
+        }
+
+        #endregion
+    }
+}

FunProcGen.WinForms/MainForm.cs
@@ -1,0 +1,10 @@
+namespace FunProcGen.WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

FunProcGen.WinForms/Program.cs
@@ -1,0 +1,12 @@
+namespace FunProcGen.WinForms
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}

djcarter85/ProcGenFun

ProcGenFun.WinForms/MainForm.Designer.cs
@@ -1,0 +1,39 @@
+﻿namespace ProcGenFun.WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Fun ProcGen";
+        }
+
+        #endregion
+    }
+}

ProcGenFun.WinForms/MainForm.cs
@@ -1,0 +1,10 @@
+namespace ProcGenFun.WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

ProcGenFun.WinForms/Program.cs
@@ -1,0 +1,12 @@
+namespace ProcGenFun.WinForms
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}