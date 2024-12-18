d0a19089a7f2bf2bb782fb66ecbeea8ee98c7f51
Dan Carter <djcarter85@gmail.com>
2024-12-18 13:03:34 +0000

    refactor extract state

--- ProcGenFun/Mazes/BinaryTree.cs
+++ ProcGenFun/Mazes/BinaryTree.cs
@@ -9,32 +9,34 @@ public static class BinaryTree
 {
     public static IDistribution<Maze> MazeDistribution(Grid grid)
     {
-        var initialState = Maze.WithAllWalls(grid);
+        var initialState = new State(Maze.WithAllWalls(grid));
 
-        IDistribution<Maze> mazeDist = Singleton.New(initialState);
+        IDistribution<State> stateDist = Singleton.New(initialState);
 
         foreach (var cell in grid.Cells)
         {
-            mazeDist = NextStepDistribution(mazeDist, grid, cell);
+            stateDist = NextStepDistribution(stateDist, grid, cell);
         }
 
-        return mazeDist;
+        return stateDist.Select(s => s.Maze);
     }
 
-    private static IDistribution<Maze> NextStepDistribution(
-        IDistribution<Maze> mazeDist, Grid grid, Cell cell)
+    private static IDistribution<State> NextStepDistribution(
+        IDistribution<State> stateDist, Grid grid, Cell cell)
     {
         var validDirections = GetValidDirections(cell, grid);
 
         return UniformDistribution.TryCreate(validDirections, out var directionDist)
-            ? (from maze in mazeDist
+            ? (from state in stateDist
                from direction in directionDist
-               select maze.RemoveWall(cell, direction))
-            : mazeDist;
+               select new State(Maze: state.Maze.RemoveWall(cell, direction)))
+            : stateDist;
     }
 
     private static IEnumerable<Direction> GetValidDirections(Cell cell, Grid grid) =>
         from dir in new[] { Direction.North, Direction.East }
         where grid.AdjacentCellOrNull(cell, dir) != null
         select dir;
+
+    private record State(Maze Maze);
 }
