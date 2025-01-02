ff61ca142ed03e491b030c6aee0422dc13814e42
Dan Carter <djcarter85@gmail.com>
2025-01-02 12:54:26 +0000

    add even more debugging

--- ProcGenFun.WinForms/MazeForm.Designer.cs
+++ ProcGenFun.WinForms/MazeForm.Designer.cs
@@ -32,6 +32,11 @@
             this.saveImagesButton = new Button();
             this.pictureBox = new PictureBox();
             this.mazeAlgorithmCombo = new ComboBox();
+            this.firstButton = new Button();
+            this.previousButton = new Button();
+            this.nextButton = new Button();
+            this.lastButton = new Button();
+            this.label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)this.pictureBox).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,7 +65,7 @@
             // 
             // pictureBox
             // 
-            this.pictureBox.Location = new Point(14, 64);
+            this.pictureBox.Location = new Point(12, 109);
             this.pictureBox.Margin = new Padding(3, 4, 3, 4);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new Size(114, 67);
@@ -76,11 +81,68 @@
             this.mazeAlgorithmCombo.Size = new Size(220, 28);
             this.mazeAlgorithmCombo.TabIndex = 3;
             // 
+            // firstButton
+            // 
+            this.firstButton.Enabled = false;
+            this.firstButton.Location = new Point(12, 73);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new Size(35, 29);
+            this.firstButton.TabIndex = 4;
+            this.firstButton.Text = "«";
+            this.firstButton.UseVisualStyleBackColor = true;
+            this.firstButton.Click += this.firstButton_Click;
+            // 
+            // previousButton
+            // 
+            this.previousButton.Enabled = false;
+            this.previousButton.Location = new Point(53, 73);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new Size(35, 29);
+            this.previousButton.TabIndex = 5;
+            this.previousButton.Text = "<";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += this.previousButton_Click;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Enabled = false;
+            this.nextButton.Location = new Point(94, 73);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new Size(35, 29);
+            this.nextButton.TabIndex = 6;
+            this.nextButton.Text = ">";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += this.nextButton_Click;
+            // 
+            // lastButton
+            // 
+            this.lastButton.Enabled = false;
+            this.lastButton.Location = new Point(135, 73);
+            this.lastButton.Name = "lastButton";
+            this.lastButton.Size = new Size(35, 29);
+            this.lastButton.TabIndex = 7;
+            this.lastButton.Text = "»";
+            this.lastButton.UseVisualStyleBackColor = true;
+            this.lastButton.Click += this.lastButton_Click;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new Point(182, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(0, 20);
+            this.label1.TabIndex = 8;
+            // 
             // MazeForm
             // 
             this.AutoScaleDimensions = new SizeF(8F, 20F);
             this.AutoScaleMode = AutoScaleMode.Font;
             this.ClientSize = new Size(873, 717);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lastButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.firstButton);
             this.Controls.Add(this.mazeAlgorithmCombo);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.saveImagesButton);
@@ -90,6 +152,7 @@
             this.Text = "ProcGen Fun: Mazes";
             ((System.ComponentModel.ISupportInitialize)this.pictureBox).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
@@ -98,5 +161,10 @@
         private Button saveImagesButton;
         private PictureBox pictureBox;
         private ComboBox mazeAlgorithmCombo;
+        private Button firstButton;
+        private Button previousButton;
+        private Button nextButton;
+        private Button lastButton;
+        private Label label1;
     }
 }
