5a88630d5fa623526d7a1655bc59140f0f636026
Dan Carter <djcarter85@gmail.com>
2025-08-06 13:30:24 +0100

    feat: add method for whether three people share a birthday on camp

--- ProcGenFun/Birthdays.cs
+++ ProcGenFun/Birthdays.cs
@@ -8,6 +8,15 @@ using RandN.Extensions;
 
 public static class Birthdays
 {
+    public static bool ThreePeopleShareBirthdayOnCamp(
+        IEnumerable<LocalDate> birthdays) =>
+        birthdays
+            .Where(b =>
+                b >= new LocalDate(2025, 07, 26) &&
+                b <= new LocalDate(2025, 08, 03))
+            .GroupBy(b => b)
+            .Any(g => g.Count() >= 3);
+
     public static IDistribution<IEnumerable<LocalDate>> BirthdaySetDist() =>
         BirthdayDist().Repeat(71);
 
