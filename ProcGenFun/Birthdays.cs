77d00316e6e0dac7bd03282648dea695178937ca
Dan Carter <djcarter85@gmail.com>
2025-08-06 13:19:42 +0100

    feat: add distribution for a set of birthdays

--- ProcGenFun/Birthdays.cs
+++ ProcGenFun/Birthdays.cs
@@ -1,12 +1,16 @@
 ﻿namespace ProcGenFun;
 
 using NodaTime;
+using ProcGenFun.Distributions;
 using RandN;
 using RandN.Distributions;
 using RandN.Extensions;
 
 public static class Birthdays
 {
+    public static IDistribution<IEnumerable<LocalDate>> BirthdaySetDist() =>
+        BirthdayDist().Repeat(71);
+
     public static IDistribution<LocalDate> BirthdayDist() =>
         from offset in Uniform.New(0, 365)
         select new LocalDate(2025, 01, 01).PlusDays(offset);
