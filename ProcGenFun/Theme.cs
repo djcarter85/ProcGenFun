e52b94b519e0a18ae111a0b16efd835feaed4651
Dan Carter <djcarter85@gmail.com>
2025-11-02 19:56:24 +0000

    feat: update theme colours to match blog

--- ProcGenFun/Theme.cs
+++ ProcGenFun/Theme.cs
@@ -5,9 +5,9 @@ using System.Drawing;
 public static class Theme
 {
     public static readonly Color White = Color.White;
-    public static readonly Color Grey300 = Color.FromArgb(180, 184, 187);
-    public static readonly Color Blue100 = Color.FromArgb(212, 234, 247);
+    public static readonly Color Grey300 = Color.FromArgb(191, 195, 197);
+    public static readonly Color Blue100 = Color.FromArgb(232, 245, 253);
     public static readonly Color Blue300 = Color.FromArgb(135, 197, 232);
     public static readonly Color Blue500 = Color.FromArgb(33, 140, 202);
-    public static readonly Color Blue900 = Color.FromArgb(3, 32, 48);
+    public static readonly Color Blue900 = Color.FromArgb(5, 50, 77);
 }
