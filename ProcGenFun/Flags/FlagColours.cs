0ae28ecb46c89c2e8be9e7806d4681d8ec67e15e
Dan Carter <djcarter85@gmail.com>
2026-02-20 21:19:07 +0000

    feat: disallow certain colour pairings

--- ProcGenFun/Flags/FlagColours.cs
+++ ProcGenFun/Flags/FlagColours.cs
@@ -20,6 +20,12 @@ public static class FlagColours
         FlagColour.Black
     ];
 
+    private static readonly IEnumerable<ColourPairing> disallowedColourPairings =
+    [
+        new(FlagColour.Red, FlagColour.Burgundy),
+        new(FlagColour.DarkBlue, FlagColour.Burgundy),
+    ];
+
     public static IDistribution<FlagColour> AllDist() => ColourDist(all);
 
     public static IDistribution<FlagColour> AllowedAdjacentToDist(FlagColour adjacentColour) => 
@@ -28,8 +34,13 @@ public static class FlagColours
     private static IDistribution<FlagColour> ColourDist(IEnumerable<FlagColour> colours) =>
         WeightedDiscreteDistribution.New(colours.Select(c => new Weighting<FlagColour>(c, GetWeighting(c))));
 
-    private static IEnumerable<FlagColour> AllowedAdjacentTo(FlagColour adjacentColour) => 
-        all.Where(c => c != adjacentColour);
+    private static IEnumerable<FlagColour> AllowedAdjacentTo(FlagColour adjacentColour) =>
+        all.Except([adjacentColour]).Except(DisallowedAdjacentTo(adjacentColour));
+
+    private static IEnumerable<FlagColour> DisallowedAdjacentTo(FlagColour adjacentColour) =>
+        disallowedColourPairings.Where(p => p.Colour1 == adjacentColour).Select(p => p.Colour2)
+            .Concat(disallowedColourPairings.Where(p => p.Colour2 == adjacentColour).Select(p => p.Colour1))
+            .Distinct();
 
     private static int GetWeighting(FlagColour colour) =>
         colour switch
@@ -47,4 +58,6 @@ public static class FlagColours
             FlagColour.Black => 3,
             _ => throw new ArgumentOutOfRangeException(nameof(colour), colour, null)
         };
+    
+    private record ColourPairing(FlagColour Colour1, FlagColour Colour2);
 }
