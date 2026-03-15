91ffd7fa455e8f38ca0508dc67eb7f8922b840b0
Dan Carter <djcarter85@gmail.com>
2026-03-15 20:35:30 +0000

    refactor: move class to matching file

---
+++ ProcGenFun/Flags/FlagPattern.cs
@@ -0,0 +1,34 @@
+namespace ProcGenFun.Flags;
+
+using Dunet;
+
+[Union]
+public partial record FlagPattern
+{
+    public partial record Solid(FlagColour Colour);
+    public partial record Canton(FlagColour Field, FlagColour CantonColour);
+    public partial record VerticalDiband(FlagColour Left, FlagColour Right);
+    public partial record HorizontalDiband(FlagColour Top, FlagColour Bottom);
+    public partial record VerticalTriband(FlagColour Left, FlagColour Middle, FlagColour Right);
+    public partial record HorizontalTriband(FlagColour Top, FlagColour Middle, FlagColour Bottom);
+    public partial record DiagonalBicolour(FlagColour Left, FlagColour Right, Diagonal Diagonal);
+    public partial record Cross(FlagColour Background, FlagColour Foreground, CrossType CrossType);
+    public partial record Saltire(FlagColour NorthSouthField, FlagColour EastWestField, FlagColour Foreground);
+    public partial record Quartered(FlagColour TopLeft, FlagColour TopRight, FlagColour BottomRight, FlagColour BottomLeft);
+    public partial record HorizontalStriped(FlagColour Colour1, FlagColour Colour2, int StripeCount);
+
+    public enum Type
+    {
+        Solid,
+        Canton,
+        VerticalDiband,
+        HorizontalDiband,
+        VerticalTriband,
+        HorizontalTriband,
+        DiagonalBicolour,
+        Cross,
+        Saltire,
+        Quartered,
+        HorizontalStriped
+    }
+}
