e5ec85d5600c2a0edc5521940ba4c5f4f0d8c334
Dan Carter <djcarter85@gmail.com>
2026-03-23 11:49:51 +0000

    feat: add charges to horizontal diband flags

--- ProcGenFun/Flags/Generation/HorizontalDibandCreator.cs
+++ ProcGenFun/Flags/Generation/HorizontalDibandCreator.cs
@@ -1,5 +1,6 @@
 namespace ProcGenFun.Flags.Generation;
 
+using ProcGenFun.Distributions;
 using ProcGenFun.Flags.Model;
 using RandN;
 using RandN.Extensions;
@@ -9,5 +10,14 @@ public static class HorizontalDibandCreator
     public static IDistribution<Flag> Dist() =>
         from top in FlagColours.AllDist()
         from bottom in FlagColours.AllowedAdjacentToDist(top)
-        select new Flag(new FlagPattern.HorizontalDiband(top, bottom), []);
+        from chargeType in ChargeTypeDist()
+        from charges in FlagChargeCreator.ChargesDist(chargeType, [top, bottom], size: 3, FlagChargeHorizontalLocation.Centre)
+        select new Flag(new FlagPattern.HorizontalDiband(top, bottom), charges);
+
+    private static IDistribution<FlagChargeShape.Type?> ChargeTypeDist() =>
+        WeightedDiscreteDistributionBuilder<FlagChargeShape.Type?>.Empty()
+            .Add(null, 10)
+            .Add(FlagChargeShape.Type.Star, 3)
+            .Add(FlagChargeShape.Type.Circle, 2)
+            .Build();
 }
