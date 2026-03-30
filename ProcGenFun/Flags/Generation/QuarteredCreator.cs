8fdd3589bd7c95a0535c1c66cac1009e8fbcd7fc
Dan Carter <djcarter85@gmail.com>
2026-03-30 21:29:08 +0100

    refactor: extract methods

--- ProcGenFun/Flags/Generation/QuarteredCreator.cs
+++ ProcGenFun/Flags/Generation/QuarteredCreator.cs
@@ -9,7 +9,13 @@ public static class QuarteredCreator
     public static IDistribution<Flag> Dist() =>
         from topLeft in FlagColours.AllDist()
         from topRight in FlagColours.AllowedAdjacentToDist(topLeft)
-        from bottomRight in FlagColours.AllowedAdjacentToDist(topRight)
-        from bottomLeft in FlagColours.AllowedAdjacentToDist([topLeft, bottomRight])
+        from bottomRight in BottomRightDist(topRight)
+        from bottomLeft in BottomLeftDist(topLeft, bottomRight)
         select new Flag(new FlagPattern.Quartered(topLeft, topRight, bottomRight, bottomLeft), []);
+
+    private static IDistribution<FlagColour> BottomRightDist(FlagColour topRight) => 
+        FlagColours.AllowedAdjacentToDist(topRight);
+
+    private static IDistribution<FlagColour> BottomLeftDist(FlagColour topLeft, FlagColour bottomRight) => 
+        FlagColours.AllowedAdjacentToDist([topLeft, bottomRight]);
 }
