850c8fb7f2875300df1757143cdbecd0fad597b6
Dan Carter <djcarter85@gmail.com>
2026-03-30 18:53:04 +0100

    feat: only fimbriate saltire if field colours are the same

--- ProcGenFun/Flags/Generation/SaltireCreator.cs
+++ ProcGenFun/Flags/Generation/SaltireCreator.cs
@@ -13,16 +13,18 @@ public static class SaltireCreator
         from fieldColoursAreSame in Bernoulli.FromRatio(4, 5)
         from eastWestfield in EastWestFieldDist(northSouthField, fieldColoursAreSame)
         from foreground in FlagColours.AllowedAdjacentToDist([northSouthField, eastWestfield])
-        from fimbriation in FimbriationDist(northSouthField, eastWestfield, foreground)
+        from fimbriation in FimbriationDist(fieldColoursAreSame, northSouthField, eastWestfield, foreground)
         select new Flag(new FlagPattern.Saltire(northSouthField, eastWestfield, foreground, fimbriation), []);
 
     private static IDistribution<FlagColour?> FimbriationDist(
-        FlagColour northSouthField, FlagColour eastWestfield, FlagColour foreground) =>
-        Bernoulli.FromRatio(1, 10)
-            .SelectMany(shouldFimbriate =>
-                shouldFimbriate
-                    ? FlagColours.AllowedAdjacentToDist([northSouthField, eastWestfield, foreground]).Nullable()
-                    : Singleton.New<FlagColour?>(null));
+        bool fieldColoursAreSame, FlagColour northSouthField, FlagColour eastWestfield, FlagColour foreground) =>
+        !fieldColoursAreSame
+            ? Singleton.New<FlagColour?>(null)
+            : Bernoulli.FromRatio(1, 10)
+                .SelectMany(shouldFimbriate =>
+                    shouldFimbriate
+                        ? FlagColours.AllowedAdjacentToDist([northSouthField, eastWestfield, foreground]).Nullable()
+                        : Singleton.New<FlagColour?>(null));
 
     private static IDistribution<FlagColour> EastWestFieldDist(
         FlagColour northSouthField, bool fieldColoursAreSame) =>
