51a91fc6be3c5d9e6e2516dd9da1eee105139314
Dan Carter <djcarter85@gmail.com>
2026-03-30 21:32:53 +0100

    feat: improve colours of quartered flags

--- ProcGenFun/Flags/Generation/QuarteredCreator.cs
+++ ProcGenFun/Flags/Generation/QuarteredCreator.cs
@@ -2,6 +2,7 @@ namespace ProcGenFun.Flags.Generation;
 
 using ProcGenFun.Flags.Model;
 using RandN;
+using RandN.Distributions;
 using RandN.Extensions;
 
 public static class QuarteredCreator
@@ -9,13 +10,22 @@ public static class QuarteredCreator
     public static IDistribution<Flag> Dist() =>
         from topLeft in FlagColours.AllDist()
         from topRight in FlagColours.AllowedAdjacentToDist(topLeft)
-        from bottomRight in BottomRightDist(topRight)
-        from bottomLeft in BottomLeftDist(topLeft, bottomRight)
+        from bottomRight in BottomRightDist(topLeft, topRight)
+        from bottomLeft in BottomLeftDist(topLeft, topRight, bottomRight)
         select new Flag(new FlagPattern.Quartered(topLeft, topRight, bottomRight, bottomLeft), []);
 
-    private static IDistribution<FlagColour> BottomRightDist(FlagColour topRight) => 
-        FlagColours.AllowedAdjacentToDist(topRight);
+    private static IDistribution<FlagColour> BottomRightDist(FlagColour topLeft, FlagColour topRight) =>
+        Bernoulli.FromRatio(1, 2)
+            .SelectMany(shouldBeSameAsTopLeft =>
+                shouldBeSameAsTopLeft
+                    ? Singleton.New(topLeft)
+                    : FlagColours.AllowedAdjacentToDist(topRight));
 
-    private static IDistribution<FlagColour> BottomLeftDist(FlagColour topLeft, FlagColour bottomRight) => 
-        FlagColours.AllowedAdjacentToDist([topLeft, bottomRight]);
+    private static IDistribution<FlagColour> BottomLeftDist(
+        FlagColour topLeft, FlagColour topRight, FlagColour bottomRight) =>
+        Bernoulli.FromRatio(1, 2)
+            .SelectMany(shouldBeSameAsTopRight =>
+                shouldBeSameAsTopRight
+                    ? Singleton.New(topRight)
+                    : FlagColours.AllowedAdjacentToDist([topLeft, bottomRight]));
 }
