69ac526ff4b33a1e6a4c1a94450fa7dec1239bc1
Dan Carter <djcarter85@gmail.com>
2026-05-09 21:20:07 +0100

    refactor: use distribution builder for canton flag charges

--- ProcGenFun/Flags/Generation/CantonFlagCreator.cs
+++ ProcGenFun/Flags/Generation/CantonFlagCreator.cs
@@ -10,15 +10,15 @@ public static class CantonFlagCreator
     public static IDistribution<Flag> Dist() =>
         from field in FlagColours.AllDist()
         from cantonColour in FlagColours.AllowedAdjacentToDist(field)
-        from chargeType in ChargeTypeDist()
-        from charges in FlagChargeCreator.ChargesDist(chargeType, [cantonColour], FlagChargeSize.Medium,  FlagChargeLocation.TopHalfLeftHalf)
+        from charges in ChargesDist(cantonColour)
         select new Flag(new FlagPattern.Canton(field, cantonColour), charges);
 
-    private static IDistribution<FlagChargeShape.Type?> ChargeTypeDist() =>
-        WeightedDiscreteDistributionBuilder<FlagChargeShape.Type?>.Empty()
-            .Add(null, 1)
-            .Add(FlagChargeShape.Type.Star, 8)
-            .Add(FlagChargeShape.Type.Plus, 4)
-            .Add(FlagChargeShape.Type.Circle, 2)
-            .Build();
+    private static IDistribution<IReadOnlyList<FlagCharge>> ChargesDist(FlagColour cantonColour) =>
+        WeightedDiscreteDistributionBuilder<IDistribution<IReadOnlyList<FlagCharge>>>.Empty()
+            .Add(FlagChargeCreator.NoChargesDist(), 1)
+            .Add(FlagChargeCreator.StarChargeDist([cantonColour], FlagChargeSize.Medium,  FlagChargeLocation.TopHalfLeftHalf), 8)
+            .Add(FlagChargeCreator.PlusChargeDist([cantonColour], FlagChargeSize.Medium,  FlagChargeLocation.TopHalfLeftHalf), 4)
+            .Add(FlagChargeCreator.CircleChargeDist([cantonColour], FlagChargeSize.Medium,  FlagChargeLocation.TopHalfLeftHalf), 2)
+            .Build()
+            .Flatten();
 }
